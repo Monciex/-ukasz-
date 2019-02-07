f38d5257d16065a47815efed549e7a5a36a75938
Monciex <45048260+Monciex@users.noreply.github.com>
2019-02-07 20:21:10 +0100

    Create Main.java

---
+++ Wektory/1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] wektor1 = {2, -1};
+        int[] wektor2 = {3, 4};
+        System.out.println(Iloczyn.iloczynSkalarny(wektor1,wektor2));
+
+
+    }
+}
