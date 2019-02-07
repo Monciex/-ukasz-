5cbbca39273d53e5b23b66a7c57eec4d3a88805a
Monciex <45048260+Monciex@users.noreply.github.com>
2019-02-07 21:05:08 +0100

    Create IloczynWektorowy.java

---
+++ Wektory/3/IloczynWektorowy.java
@@ -0,0 +1,18 @@
+public class IloczynWektorowy {
+
+    public int Iloczyn1(int b,int f,int c,int e) {
+        int A = b*f;
+        int B = c*e;
+        return B-A;
+    }
+    public int Iloczyn2(int c,int d,int a, int f) {
+        int A=c*d;
+        int B = a*f;
+        return B-A;
+    }
+    public int Iloczyn3(int a,int e,int b,int d) {
+        int A = a*e;
+        int B = b*d;
+        return B-A;
+    }
+}
